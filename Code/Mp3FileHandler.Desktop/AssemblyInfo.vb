1f71096301c2af6caadb0fb004c26f249785a5b1
Fernando Andrade <fernando@hrestart.com.br>
2022-05-11 15:54:35 -0300

    v1.22.3

--- Code/Mp3FileHandler.Desktop/AssemblyInfo.vb
+++ Code/Mp3FileHandler.Desktop/AssemblyInfo.vb
@@ -29,5 +29,5 @@ Imports System.Runtime.InteropServices
 ' You can specify all the values or you can default the Build and Revision Numbers 
 ' by using the '*' as shown below:
 
-<Assembly: AssemblyVersion("1.22.2")>
-<Assembly: AssemblyFileVersion("1.22.2")>
+<Assembly: AssemblyVersion("1.22.3")>
+<Assembly: AssemblyFileVersion("1.22.3")>
